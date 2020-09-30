2c4bec18eeade8e1ef4a24158f1817146885b996
Anastasiia Krasnoselska <akrasnosel@e1r8p5.unit.ua>
2020-09-30 18:00:21 +0300

    New Script

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
+echo 'export PATH="/Users/akrasnosel/.brew/opt/qt/bin:$PATH"' >> ~/.zshrc
+export LDFLAGS="-L/Users/akrasnosel/.brew/opt/qt/lib"
+export CPPFLAGS="-I/Users/akrasnosel/.brew/opt/qt/include"
+export PKG_CONFIG_PATH="/Users/akrasnosel/.brew/opt/qt/lib/pkgconfig"
 cmake . -Bbuild -Wdev -Werror=dev && cmake --build ./build
